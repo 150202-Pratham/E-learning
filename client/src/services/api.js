b28395dd89b1c38d731b9c0c747565028c96ac9e
KUMAR GARG <122441585+kumargarg82561@users.noreply.github.com>
2025-04-23 23:03:23 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Update to your backend URL
+  baseURL: "https://e-learning-ahkp.onrender.com", // Update to your backend URL
 });
 
 API.interceptors.request.use((config) => {
