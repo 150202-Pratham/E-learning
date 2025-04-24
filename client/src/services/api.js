487aabd6426fbbabdec2439ff2a2613e7ece8fc6
Pratham Garg <140713281+150202-Pratham@users.noreply.github.com>
2025-04-24 18:55:56 +0530

    Update api.js
    I have changes the base url address of the API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://e-learning-ahkp.onrender.com", // Update to your backend URL
+  baseURL: "https://e-learning-backend-ecvw.onrender.com/api", // Update to your backend URL
 });
 
 API.interceptors.request.use((config) => {
